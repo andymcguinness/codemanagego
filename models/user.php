fee96222864f3a9de7e96e14488eff5b9490bb1a
Maia <maiamcguinness@gmail.com>
2013-10-11 20:20:08 -0400

    Added a User class

---
+++ models/user.php
@@ -0,0 +1,77 @@
+<?php
+/* This is the User class */
+
+class User {
+    
+    /* Protected properties */
+    protected $id;
+    protected $userName;
+    protected $lastName;
+    protected $firstName;
+    protected $email;
+    protected $isManager;
+    
+    /* Entity relationship */
+    protected $assignments;
+    
+    public function setAssignment(array $assignments) {
+        $this->assignments = $assignments;
+    }
+    
+    public function projectsAssigned(Project $project) {
+        $this->assignments[] = $project;
+    }
+    
+    /* Constructor */
+    
+    public function __construct($userName, $lastName, $firstName, $email){ // You need all information to sign up
+        
+        // Testing to make sure first & last names are strings
+        $this->setFirstName($firstName);
+        $this->setLastName($lastName);
+        
+        $this->userName = $userName;
+        $this->email = $email;
+    }
+    
+    /* Interface methods */
+    
+    // Function to return any property (DRY)
+    public function __get($item) {
+        return $this->$item;
+    }
+    
+    // Does what it says on the box
+    public function setUserName($userName) {
+        $this->userName = $userName;
+    }
+    
+    public function setFirstName($firstName) {
+        if (gettype($firstName) == 'string') {
+            $this->firstName = $firstName;            
+        } else {
+            user_error("You did not enter a valid first name. Try again.");
+        }
+    }
+    
+    public function setLastName($lastName) {
+        if (gettype($lastName) == 'string') {
+            $this->lastName = $lastName;            
+        } else {
+            user_error("You did not enter a valid last name. Try again.");
+        }
+    }
+    
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    
+    public function setIsManager($isManager) {
+        if (gettype($isManager) == 'boolean') {
+            $this->isManager = $isManager;            
+        } else {
+            user_error('Incorrect data type for variable isManager. Please enter a bool and try again.');
+        }
+    }
+}
+?>
