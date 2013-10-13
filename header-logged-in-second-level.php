04143821e8d837b1e2f917762cabeb8d06b3f9ef
Maia <maiamcguinness@gmail.com>
2013-10-12 22:16:32 -0400

    Missed files in previous commit

---
+++ header-logged-in-second-level.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Code, Manage, Go!</title>
+        <meta name="description" content="Website for the Code, Manage, Go! platform.">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="../css/normalize.css">
+        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="../css/foundation.css">
+
+        <script src="js/vendor/custom.modernizr.js"></script>
+    </head>
+    
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <header class="row">
+            <div class="large-10 large-uncentered small-6 small-centered columns">
+                <a href="../index.php">
+                    <div class="row">
+                        <div class="large-1 small-12 columns"><div id="hexaheader" class="logo">!</div></div>
+                        <div class="large-11 columns hide-for-small"><div class="title">Code, Manage, Go!</div></div>
+                    </div>
+                </a>
+            </div>
+        </header>
