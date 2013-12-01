86b04cc6aa69ba24ba09f829d3099258215f0b0f
Cat McGuinness <maiamcguinness@gmail.com>
2013-12-01 03:32:38 -0500

    Added controllers for users and files.

---
+++ mvc/application/controllers/users.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+
+}
+
+/* End of file users.php */
+/* Location: ./application/controllers/users.php */
