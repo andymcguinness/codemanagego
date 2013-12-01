99b1a49b4fa47cb8f523a6d387887002305aaf77
Cat McGuinness <maiamcguinness@gmail.com>
2013-12-01 03:48:55 -0500

    Added the rest of the controllers and updated some existing ones.

---
+++ mvc/application/controllers/projects.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Projects extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+
+}
+
+/* End of file projects.php */
+/* Location: ./application/controllers/projects.php */
