c6b9b0cb4f7765fda42e3fca839aa44a27ceb97b
Cat McGuinness <maiamcguinness@gmail.com>
2013-12-12 17:59:01 -0500

    Fixed footer width, and updated the styling on the signup and signin pages in line with new design.

--- mvc/application/views/login.php
+++ mvc/application/views/login.php
@@ -1,6 +1,6 @@
 <div class="row">
-    <div class="small-10 small-centered columns signin">
-        <h1>Sign in to access your account.</h1>
+    <div class="large-12 large-uncentered small-10 small-centered columns full-width-wrapper signin">
+        <h3>Sign in to access your account.</h3>
 
         <?php if(isset($error)) {
             echo '<div class="error">' . $error . '</div>';
@@ -13,19 +13,19 @@
         <?php echo form_open('login') ?>
 
             <div class="row">
-                <div class="small-8 small-centered columns">
+                <div class="large-8 large-centered small-12 small-centered columns">
                     <label>Username: <input name="usr_username" type="text" value="<?php echo set_value('usr_username'); ?>" /></label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="small-8 small-centered columns">
+                <div class="large-8 large-centered small-12 small-centered columns">
                     <label>Password: <input name="usr_password" type="text" value="<?php echo set_value('usr_password'); ?>" /></label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="small-3 small-centered columns">
+                <div class="large-2 large-centered medium-2 medium-centered small-5 small-centered columns">
                     <button class="cmg-btn-large" type="submit">Sign In</button>
                 </div>
             </div>
--- mvc/application/views/register.php
+++ mvc/application/views/register.php
@@ -1,39 +1,39 @@
 <div class="row">
-    <div class="small-10 small-centered columns signup">
-        <h1>Sign up for this sweet service!</h1>
+    <div class="large-12 large-uncentered small-10 small-centered columns full-width-wrapper signup">
+        <h3>Sign up for this sweet service!</h3>
 
         <?php echo validation_errors(); ?>
 
         <?php echo form_open('register') ?>
             <div class="row">
-                <div class="small-4 push-2 columns">
+                <div class="large-4 large-push-2 small-12 columns">
                     <label>First Name: <input name="usr_firstname" type="text" value="<?php echo set_value('usr_firstname'); ?>" /></label>
                 </div>
-                <div class="small-4 pull-2 columns">
+                <div class="large-4 large-pull-2 small-12 columns">
                     <label>Last Name: <input name="usr_lastname" type="text" value="<?php echo set_value('usr_lastname'); ?>" /></label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="small-8 small-centered columns">
+                <div class=" large-8 large-centered small-12 small-centered columns">
                     <label>Username: <input name="usr_username" type="text" value="<?php echo set_value('usr_username'); ?>" /></label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="small-8 small-centered columns">
+                <div class="large-8 large-centered small-12 small-centered columns">
                     <label>Password: <input name="usr_password" type="text" value="<?php echo set_value('usr_password'); ?>" /></label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="small-8 small-centered columns">
+                <div class="large-8 large-centered small-12 small-centered columns">
                     <label>Email: <input name="usr_email" type="text" value="<?php echo set_value('usr_email'); ?>" /></label>
                 </div>
             </div>
 
             <div class="row">
-                <div class="small-3 small-centered columns">
+                <div class="large-2 large-centered medium-2 medium-centered small-5 small-centered columns">
                     <button class="cmg-btn-large" type="submit">Sign Up</button>
                 </div>
             </div>
